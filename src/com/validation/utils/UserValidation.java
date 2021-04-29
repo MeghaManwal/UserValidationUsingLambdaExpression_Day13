22a5d7bf183fdc94443ed493aa01f4c3d8d2c98e
MeghaManwal <81420523+MeghaManwal@users.noreply.github.com>
2021-04-29 19:46:35 +0530

    Refactor the code to use Lambda Function to validate User Entry

--- src/com/validation/utils/UserValidation.java
+++ src/com/validation/utils/UserValidation.java
@@ -6,46 +6,46 @@ import java.util.regex.Pattern;
 
 public class UserValidation {
 	
-		public static boolean isValidFirstname(String firstname) {
+	public static boolean isValidFirstname(String firstname) {
 	
 	        String pattern1="^[A-Z]{1}[A-Z a-z]{2,}$";
 	        Predicate<String> predicate=p->firstname.matches(pattern1);
 	        boolean value=predicate.test(firstname);
 	        return value; 
-		}
+	}
 		
-	    public static boolean isValidLastname(String lastname) {
+        public static boolean isValidLastname(String lastname) {
 	
 	        String pattern2="^[A-Z]{1}[A-Z a-z]{2,}$";
 	        Predicate<String> predicate=p->lastname.matches(pattern2);
 	        boolean value=predicate.test(lastname);
 	        return value; 
-	    }
+	}
 
-       public static boolean isValidEmailID(String emailId) {
+        public static boolean isValidEmailID(String emailId) {
 
 	        String pattern3="^[A-Z a-z 0-9]+([._+-][0-9 a-z A-Z]+)*@[0-9 a-z A-Z]+.[a-z A-Z]{2,3}([.][a-z A-Z]{2})*$";
 	        Predicate<String> predicate=p->emailId.matches(pattern3);
 	        boolean value=predicate.test(emailId);
 	        return value;
-       }
+        }
 
 
-       public static boolean isValidPhoneNumb(String phoneNumber) {
+        public static boolean isValidPhoneNumb(String phoneNumber) {
 
 	        String pattern4="^[+][0-9]{1,}\\s?[1-9]{1}[0-9]{9}$";
 	        Predicate<String> predicate=p->phoneNumber.matches(pattern4);
 	        boolean value=predicate.test(phoneNumber);
 	        return value;
-       }
+        }
 
-       public static boolean isValidPassworD(String password) {
+        public static boolean isValidPassworD(String password) {
 
 	        String pattern5="^(?=[0-9 A-Z a-z !@#$%^&*();:]{8,}$)(?=.*?[A-Z]{1,})(?=.*?[0-9]{1,})(?=.*?[!@#$%^&*();:]{1,}).*$";
 	        Predicate<String> predicate=p->password.matches(pattern5);
 	        boolean value=predicate.test(password);
 	        return value;
-       }
+        }
 
         
         public static void main(String[] args) {
